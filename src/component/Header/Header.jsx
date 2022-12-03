83239df27371d978f0433dc52505426592ff014e
unknown <tamiloreolaniyi@gmail.com>
2022-12-03 11:09:13 +0100

    first push

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
+import {faBed, faCar, faPlane, faTaxi} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import './Header.css'
 
 function Header() {
   return (
     <div className='header'>
-      <div className="headerList">
-        <div className="headerListItem"></div>
+        <div className="headerContainer">
+        <div className="headerList">
+        <div className="headerListItem">
+        <FontAwesomeIcon icon={faBed} />
+        <span>Stays</span>
+        </div>
+        <div className="headerListItem">
+        <FontAwesomeIcon icon={faPlane} />
+        <span>Flights</span>
+        </div>
+        <div className="headerListItem">
+        <FontAwesomeIcon icon={faCar} />
+        <span>Car Rentals</span>
+        </div>
+        <div className="headerListItem">
+        <FontAwesomeIcon icon={faBed} />
+        <span>Attractions</span>
+        </div>
+        <div className="headerListItem">
+        <FontAwesomeIcon icon={faTaxi} />
+        <span>Airport taxis</span>
+        </div>
       </div>
+        </div>
+
     </div>
   )
 }
